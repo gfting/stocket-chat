0ba330972d078e8f6bfb9fba95082fe6aa5bcf84
Gabriel Ting <gabrielfting@gmail.com>
2020-10-24 11:45:58 -0500

    Add basic tutorial implementation

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    io.emit('user connected')
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('user disconnected')
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+// Start the server on port 3000
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
