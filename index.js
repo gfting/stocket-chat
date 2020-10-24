61a8eedea79dd7cf685264a85decb450906ee7e7
Gabriel Ting <gabrielfting@gmail.com>
2020-10-24 11:49:33 -0500

    Add default PORT for Heroku

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ io.on('connection', (socket) => {
 });
 
 // Start the server on port 3000
-server.listen(3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('listening on *:3000');
 });
