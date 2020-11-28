f74ab904305b8131a160ed56d6ea1308adc11cfb
Gabriel Ting <gabrielfting@gmail.com>
2020-11-28 11:45:37 -0600

    Add nickname and headers

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
+import { Theme } from './Theme'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Theme>
     <App />
+    </Theme>
   </React.StrictMode>,
   document.getElementById('root')
 );
